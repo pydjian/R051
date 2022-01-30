e8758694ee412cebcd01ed4bca99337b012acc92
marukoy <61203201+marukoy@users.noreply.github.com>
2022-01-31 02:07:53 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ubus call version set_sn_imei_info '{"imei":"0"}'
-wget http://192.168.1.5/R051.bin -O /tmp/firmware.bin
+wget https://github.com/marukoy/R051/blob/main/R051.bin?raw=true -O /tmp/firmware.bin
 mtd -r write /tmp/firmware.bin /dev/mtd4
 
