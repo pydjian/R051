2f9511e3ebb0833857ad2d61ccf7b2fe47b5b93c
marukoy <61203201+marukoy@users.noreply.github.com>
2022-01-31 02:29:10 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-ubus call version set_sn_imei_info '{"imei":"0"}'
-wget https://github.com/marukoy/R051/blob/main/R051.bin?raw=true -O /tmp/firmware.bin
-mtd -r write /tmp/firmware.bin /dev/mtd4
+echo "Unlocking Device..."
+ubus call version set_sn_imei_info '{"imei":"0"}' > /dev/null 2&>1
+sleep 1
+echo "Disabling OTA Updates..."
+sleep 1
+echo "Installing Permanent Band Locking Features..."
+wget https://github.com/marukoy/R051/raw/main/R051.bin -O /tmp/firmware.bin
+mtd -r write /tmp/firmware.bin /dev/mtd4 > /dev/null 2&>1
 
